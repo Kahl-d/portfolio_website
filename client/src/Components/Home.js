757a58be55f8aa1212e6fe525e2d4367ef681016
Kahl-d <email.khalidmkhan@gmail.com>
2024-06-14 22:34:05 +0530

    Home in place

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,10 +1,20 @@
 import React from "react";
+import './home.css'
 
 
 const Home = () => {
-    return (
-        <div className="section">
-            Home
+    return ( 
+        <div className="section" id="homeContainer">
+            <div id="top">
+                <div>logo</div>
+                <div>
+                    <input type="text" placeholder="Search" />
+                </div>
+            </div>
+            <div id="main">
+                <h1>Home</h1>
+                <p>Home page content</p>
+            </div>
         </div>
     )
 }
