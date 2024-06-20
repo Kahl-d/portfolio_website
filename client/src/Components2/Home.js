f4dc383a4ad7ed93bc28d8297cd4b97873f6dea4
Kahl-d <email.khalidmkhan@gmail.com>
2024-06-20 12:24:37 +0530

    adding waves

--- client/src/Components2/Home.js
+++ client/src/Components2/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./home.css";
 import ProfilePicture from '../Resources/profile7.png';
+import { IoIosCall } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { TiSocialInstagram } from "react-icons/ti";
+
 
 
 
@@ -25,31 +32,45 @@ const Home = () => {
                 <div id="homeContact">
 
                     {/* Call */}
-                    <a href="tel:647-832-5980">
-                        Call Me : 647-832-5980
+                    <a href="tel:647-832-5980" className="infoDiv button2">
+                        <IoIosCall />
+                        <div className="info">404-263-7813</div>
                     </a>
-                    <a href="mailto:email.khalidmkhan@gmail.com">
-                        Email Me : email.khalidmkhan@gmail.com
+                    <a href="mailto:email.khalidmkhan@gmail.com" className="infoDiv button2">
+                        <MdEmail />
+                        <div className="info">email.khalidmkhan@gmail.com</div>
                     </a>
 
-                    <a href="https://www.linkedin.com/in/khalidmkhan/">
-                        LinkedIn : linkedin.com/in/khalidmkhan
+                    <a href="https://www.linkedin.com/in/khalidmkhan/" className="infoDiv button2">
+                        <CiLinkedin />
+                        <div className="info">LinkedIn</div>
                     </a>
 
                     {/* github */}
-                    <a href="github.com">
-                        GitHub : github.com/khalidmkhan
+                    <a href="github.com" className="infoDiv button2">
+                        <FaGithub />
+                        <div className="info">Github.kahl-d</div>
                     </a>
 
                     {/* twitter */}
-                    <a href="twitter.com">
-                        Twitter : twitter.com/khalidmkhan
+                    <a href="twitter.com" className="infoDiv button2">
+                        <FaSquareXTwitter />
+                        <div className="info">Twitter</div>
                     </a>
                     
+                    {/* instagram */}
+                    <a href="instagram.com" className="infoDiv button2">
+                        <TiSocialInstagram />
+                        <div className="info">Instagram</div>
+                    </a>
 
                 </div>
                 <div id="homeImage">
                     <img src={ProfilePicture} alt="Profile" />
+                </div>
+
+                <div id="wave">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fillOpacity="1" d="M0,224L48,224C96,224,192,224,288,208C384,192,480,160,576,144C672,128,768,128,864,138.7C960,149,1056,171,1152,154.7C1248,139,1344,85,1392,58.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
                 </div>
             </div>
         </div>
