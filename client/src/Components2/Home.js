f20507ab75030dd6df22789867b0983725678486
Kahl-d <email.khalidmkhan@gmail.com>
2024-06-21 00:03:47 +0530

    fixed nav  btn, removed button2 class

--- client/src/Components2/Home.js
+++ client/src/Components2/Home.js
@@ -32,34 +32,34 @@ const Home = () => {
                 <div id="homeContact">
 
                     {/* Call */}
-                    <a href="tel:404-263-7813" className="infoDiv button2">
+                    <a href="tel:404-263-7813" className="infoDiv">
                         <IoIosCall />
                         <div className="info">(404) 263 7813</div>
                     </a>
-                    <a href="mailto:email.khalidmkhan@gmail.com" className="infoDiv button2">
+                    <a href="mailto:email.khalidmkhan@gmail.com" className="infoDiv">
                         <MdEmail />
                         <div className="info">email.khalidmkhan@gmail.com</div>
                     </a>
 
-                    <a href="https://www.linkedin.com/in/khalid-mehtab-khan-0982b1173/" className="infoDiv button2">
+                    <a href="https://www.linkedin.com/in/khalid-mehtab-khan-0982b1173/" className="infoDiv">
                         <CiLinkedin />
                         <div className="info">www.linkedin.com/in/khalid-mehtab-khan</div>
                     </a>
 
                     {/* github */}
-                    <a href="https://github.com/Kahl-d" className="infoDiv button2">
+                    <a href="https://github.com/Kahl-d" className="infoDiv">
                         <FaGithub />
                         <div className="info">github.com/Kahl-d</div>
                     </a>
 
                     {/* twitter */}
-                    <a href="https://x.com/ashvatthamaa" className="infoDiv button2">
+                    <a href="https://x.com/ashvatthamaa" className="infoDiv">
                         <FaSquareXTwitter />
                         <div className="info">x.com/ashvatthamaa</div>
                     </a>
                     
                     {/* instagram */}
-                    <a href="https://www.instagram.com/ashvatthama.__/" className="infoDiv button2">
+                    <a href="https://www.instagram.com/ashvatthama.__/" className="infoDiv">
                         <TiSocialInstagram />
                         <div className="info">@ashvatthama.__</div>
                     </a>
