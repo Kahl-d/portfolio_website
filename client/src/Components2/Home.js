9e2f867f7ba15470311f0dc4e9d167c66f643cd9
Kahl-d <email.khalidmkhan@gmail.com>
2024-06-30 23:05:00 +0530

    routine push working good

--- client/src/Components2/Home.js
+++ client/src/Components2/Home.js
@@ -24,14 +24,14 @@ const Home = () => {
 
                 </div>
 
-                <div id="homeAbout">
+                {/* <div id="homeAbout">
                     <p>
                     Innovative data science enthusiast with a keen aptitude for identifying patterns and insights within numbers and data. Passionate
                     about leveraging IT solutions in public sector, with proven ability to adapt and excel in collaborative, cross-departmental projects.
                     Driven towards advancing technology through creative solutions and meticulous learning. Excels in applying comprehensive
                     tatistical analysis and machine learning techniques to develop and optimize predictive models and AI-driven applications.
                     </p>
-                </div>
+                </div> */}
 
                 <div id="homeContact">
 
@@ -74,7 +74,7 @@ const Home = () => {
                 </div>
 
                 <div id="wave">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#444444" fillOpacity="1" d="M0,224L48,224C96,224,192,224,288,208C384,192,480,160,576,144C672,128,768,128,864,138.7C960,149,1056,171,1152,154.7C1248,139,1344,85,1392,58.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFFFFF" fillOpacity="1" d="M0,224L48,224C96,224,192,224,288,208C384,192,480,160,576,144C672,128,768,128,864,138.7C960,149,1056,171,1152,154.7C1248,139,1344,85,1392,58.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
                 </div>
             </div>
         </div>
