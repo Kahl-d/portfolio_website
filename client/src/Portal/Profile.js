d2f54272a1bfedd50e4542a91673efa1fa76e60d
Kahl-d <email.khalidmkhan@gmail.com>
2024-07-10 16:35:24 +0530

    basic fixes, structure in place

--- client/src/Portal/Profile.js
+++ client/src/Portal/Profile.js
@@ -1,16 +1,14 @@
 import React from "react";
 import './profile.css';
 
-import ProfilePicture from '../Resources/profile.png';
+import ProfilePicture from '../Resources/profile7.png';
 
 
 const Profile = () =>{
     return (
         <div id="profileContainer">
-            <div id="profileImageDiv">
-                <img src={ProfilePicture} alt="Profile" />
-                <span className="name">Hi, I'm Khalid</span>
-            </div>
+            <h1>Hi, I'm Khalid M Khan</h1>
+
         </div>
     )
 }
