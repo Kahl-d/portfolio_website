365b809c9aa41bc9941b43f91e95fd5a3fc41c9e
Kahl-d <email.khalidmkhan@gmail.com>
2024-07-12 22:39:39 +0530

    final portfolio pages

---
+++ client/src/Final/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import './app.css';
+import { IoHomeSharp } from "react-icons/io5";
+import { GrTools } from "react-icons/gr";
+import { GiFilmProjector } from "react-icons/gi";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
+
+const App = () => {
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const pages = [
+        <div>Page 1</div>,
+        <div>Page 2</div>,
+        <div>Page 3</div>,
+        <div>Page 4</div>
+    ];
+
+    return (
+        <div id="appContainer">
+            <div id="appContent">
+                {pages[currentPage]}
+            </div>
+
+            <div id="appFooter">
+                <div id="navBar">
+                    <div className={`navItem ${currentPage === 0 ? 'left' : ''}`}></div>
+                    <div className={`navItem ${currentPage === 0 ? 'active' : ''} ${currentPage === 1 ? 'left' : ''}`} onClick={() => setCurrentPage(0)}>
+                        <IoHomeSharp />
+                        <span className="navText">Home</span>
+                    </div>
+                    <div className={`navItem ${currentPage === 1 ? 'active' : ''} ${currentPage === 0 ? 'right' : ''} ${currentPage === 2 ? 'left' : ''}`} onClick={() => setCurrentPage(1)}>
+                        <GrTools />
+                        <span className="navText">Skills</span>
+                    </div>
+                    <div className={`navItem ${currentPage === 2 ? 'active' : ''} ${currentPage === 1 ? 'right' : ''} ${currentPage === 3 ? 'left' : ''}`} onClick={() => setCurrentPage(2)}>
+                        <GiFilmProjector />
+                        <span className="navText">Projects</span>
+                    </div>
+                    <div className={`navItem ${currentPage === 3 ? 'active' : ''} ${currentPage === 2 ? 'right' : ''}`} onClick={() => setCurrentPage(3)}>
+                        <MdOutlinePermContactCalendar />
+                        <span className="navText">Contact</span>
+                    </div>
+                    <div className={`navItem ${currentPage === 3 ? 'right' : ''}`}></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Components4/App";
+import App from "./Final/App";
 
 
 const root = document.getElementById("root");
