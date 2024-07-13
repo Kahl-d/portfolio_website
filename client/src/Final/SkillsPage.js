4c1679580a28f532d8abf973ce459f16499a3bd2
Kahl-d <email.khalidmkhan@gmail.com>
2024-07-13 11:06:16 +0530

    finally files are fixed

--- client/src/Final/SkillsPage.js
+++ client/src/Final/SkillsPage.js
@@ -95,7 +95,7 @@ const SkillsPage = () => {
                     <img
                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnI284a7-1rgJxedA-Aus1sFMZz0FdLyF7-g&s"
                         alt="projects"
-                        id="headerImg"
+                        id="headerImgs"
                         className={isScrolled ? 'scrolled' : ''}
                     />
                     <h1 className={isScrolled ? 'scrolled' : ''}>Skills</h1>
